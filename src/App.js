c04f02ae2fb06771c91342034e513fa92ccfceec
Zaharcheniavitaly <98647075+Zaharcheniavitaly@users.noreply.github.com>
2023-02-21 18:52:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import './index.scss';
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+  // let count = 0;
+
+  function onClickPlus() {
+    setCount(count + 1);
+    console.log("count>>", count);
+  }
+
+  function onClickMinus() {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      setCount((count = 0));
+    }
+
+    console.log("count>>", count);
+  }
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{count}</h1>
+        <button onClick={onClickMinus} className="minus">
+          - Минус
+        </button>
+        <button onClick={onClickPlus} className="plus">
+          Плюс +
+        </button>
       </div>
     </div>
   );
