8f12f017e52494a0f3831baf189680c9bf539c5b
Zaharcheniavitaly <98647075+Zaharcheniavitaly@users.noreply.github.com>
2023-02-21 21:36:05 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function Result({ correct }) {
       <h2>
         Вы отгадали {correct} ответа из {questions.length}
       </h2>
-      <button>Попробовать снова</button>
+      <a href="/"><button>Попробовать снова</button></a>
     </div>
   );
 }
